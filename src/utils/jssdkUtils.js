f8dcd21b6502926afbc988426c36528c5e23f308
maybe <1013480204@qq.com>
2020-04-27 17:23:37 +0800

    移除第三方 commonjs  jssdk， 自定义commonjs jssdk

--- src/utils/jssdkUtils.js
+++ src/utils/jssdkUtils.js
@@ -1,4 +1,5 @@
-var wx = require('weixin-js-sdk')
+// var wx = require('weixin-js-sdk')
+var wx = require('./jssdk')
 wx.config({
   debug: true, // 开启调试模式,调用的所有api的返回值会在客户端alert出来，若要查看传入的参数，可以在pc端打开，参数信息会通过log打出，仅在pc端时才会打印。
   appId: 'wxf8b4f85f3a794e77', // 必填，公众号的唯一标识
