af3d271cd15ef3d6ad122b65948cb67159bfa3da
maybe <1013480204@qq.com>
2020-04-29 15:21:59 +0800

    添加分页混合，完善list demo

---
+++ src/utils/mixins/page.js
@@ -0,0 +1,31 @@
+export default {
+  data () {
+    return {
+      loading: false,    // 是否加载中
+      refreshing: false, // 是否刷新中
+      finished: false, // 是否加载完成
+      page: 1,  // 页码
+      size: 10, // 条数
+      total: 0, // 总数
+      list: []  // 数据源 
+    }
+  },
+  methods: {
+    /**
+     * 重置分页
+     * @param {number} size 自定义页码 默认 10
+     */
+    resetPageData (size = 10) {
+      this.refreshing = false;
+      this.loading = false;
+      this.finished = false;
+      this.page = 1;
+      this.total = 0;
+      this.size = size;
+      this.list.length = 0;
+    },
+    async _getList () {
+      // 请求列表数据
+    }
+  }
+}
--- src/views/pages/ListDemo.vue
+++ src/views/pages/ListDemo.vue
@@ -1,26 +1,28 @@
 <template>
-  <van-list v-model="loading"
-            :finished="finished"
-            finished-text="没有更多了"
-            @load="onLoad">
-    <van-cell v-for="item in list"
-              :key="item"
-              :title="item" />
-  </van-list>
+  <van-pull-refresh v-model="refreshing"
+                    @refresh="onRefresh">
+    <van-list v-model="loading"
+              :finished="finished"
+              finished-text="没有更多了"
+              @load="_getList">
+      <van-cell v-for="item in list"
+                :key="item"
+                :title="item" />
+    </van-list>
+  </van-pull-refresh>
 </template>
 
 <script>
-import { List, Cell } from "vant";
-
+import { List, Cell, PullRefresh } from "vant";
+import pageMixins from '@/utils/mixins/page'
 export default {
+  mixins: [pageMixins],
   props: {
 
   },
   data () {
     return {
-      list: [],
-      loading: false,
-      finished: false,
+      list: []
     };
   },
   computed: {
@@ -36,27 +38,44 @@ export default {
 
   },
   methods: {
-    onLoad () {
+    async _getList () {
       // 异步更新数据
-      // setTimeout 仅做示例，真实场景中一般为 ajax 请求
-      setTimeout(() => {
-        for (let i = 0; i < 10; i++) {
-          this.list.push(this.list.length + 1);
-        }
+      console.log('页码：', this.page)
+      // 将 loading 设置为 true，表示处于加载状态
+      this.loading = true;
+      await new Promise((res, rej) => {
+        // setTimeout 仅做示例，真实场景中一般为 ajax 请求
+        setTimeout(() => {
+          for (let i = 0; i < 10; i++) {
+            this.list.push(this.list.length + 1);
+          }
+          // 加载状态结束
+          this.loading = false;
 
-        // 加载状态结束
-        this.loading = false;
-
-        // 数据全部加载完成
-        if (this.list.length >= 40) {
-          this.finished = true;
-        }
-      }, 1000);
+          // 页码+1
+          this.page++;
+          // 数据全部加载完成
+          if (this.list.length >= 40) {
+            this.finished = true;
+          }
+          res()
+        }, 1000);
+      })
+    },
+    async onRefresh () {
+      // 清空列表数据
+      this.resetPageData()
+      // 重新加载数据
+      this.refreshing = true;
+      await this._getList();
+      this.refreshing = false;
     }
   },
   components: {
     [List.name]: List,
-    [Cell.name]: Cell
+    [Cell.name]: Cell,
+    [PullRefresh.name]: PullRefresh
+
   },
 };
 </script>
