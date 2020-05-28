ecbe8834c4f3b836bc2f7b5286c4c2f581ce195a
maybe <1013480204@qq.com>
2020-05-28 12:53:08 +0800

    修复页面多次刷新获取dpr异常的问题。

--- src/assets/js/rem.js
+++ src/assets/js/rem.js
@@ -1,43 +1,51 @@
-(function flexible(window, document) {
-  var docEl = document.documentElement
-  var dpr = window.devicePixelRatio || 1
 
-  // adjust body font size
-  function setBodyFontSize() {
-    if (document.body) {
-      document.body.style.fontSize = 12 * dpr + 'px'
-    } else {
-      document.addEventListener('DOMContentLoaded', setBodyFontSize)
+// 创建一个延迟加载函数等待页面加载完成执行
+setTimeout(() => {
+  setRem();
+}, 1)
+
+function setRem () {
+  //初始化
+  (function flexible (window, document) {
+    var docEl = document.documentElement
+    var dpr = window.devicePixelRatio || 1
+    // adjust body font size
+    function setBodyFontSize () {
+      if (document.body) {
+        document.body.style.fontSize = 12 * dpr + 'px'
+      } else {
+        document.addEventListener('DOMContentLoaded', setBodyFontSize)
+      }
     }
-  }
-  setBodyFontSize()
+    setBodyFontSize()
 
-  // set 1rem = viewWidth / 10
-  function setRemUnit() {
-    var rem = docEl.clientWidth / 10
-    docEl.style.fontSize = rem + 'px'
-  }
-
-  setRemUnit()
-
-  // reset rem unit on page resize
-  window.addEventListener('resize', setRemUnit)
-  window.addEventListener('pageshow', function(e) {
-    if (e.persisted) {
-      setRemUnit()
+    // set 1rem = viewWidth / 10
+    function setRemUnit () {
+      var rem = docEl.clientWidth / 10
+      docEl.style.fontSize = rem + 'px'
     }
-  })
 
-  // detect 0.5px supports
-  if (dpr >= 2) {
-    var fakeBody = document.createElement('body')
-    var testElement = document.createElement('div')
-    testElement.style.border = '.5px solid transparent'
-    fakeBody.appendChild(testElement)
-    docEl.appendChild(fakeBody)
-    if (testElement.offsetHeight === 1) {
-      docEl.classList.add('hairlines')
+    setRemUnit()
+
+    // reset rem unit on page resize
+    window.addEventListener('resize', setRemUnit)
+    window.addEventListener('pageshow', function (e) {
+      if (e.persisted) {
+        setRemUnit()
+      }
+    })
+
+    // detect 0.5px supports
+    if (dpr >= 2) {
+      var fakeBody = document.createElement('body')
+      var testElement = document.createElement('div')
+      testElement.style.border = '.5px solid transparent'
+      fakeBody.appendChild(testElement)
+      docEl.appendChild(fakeBody)
+      if (testElement.offsetHeight === 1) {
+        docEl.classList.add('hairlines')
+      }
+      docEl.removeChild(fakeBody)
     }
-    docEl.removeChild(fakeBody)
-  }
-})(window, document)
+  })(window, document)
+}
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+
 // 初始化样式
 import '@/assets/css/reset.css'
 // 移动端rem适配js
