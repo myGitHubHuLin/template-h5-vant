0c30d77e074fd18dc949d4bc9a8da740a22957d7
maybe <1013480204@qq.com>
2020-06-22 17:37:37 +0800

    修复bug

--- src/main.js
+++ src/main.js
@@ -1,44 +1,44 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 // 初始化样式
-import '@/assets/css/reset.css'
+import "@/assets/css/reset.css";
 // 移动端rem适配js
-import '@/assets/js/rem.js'
+import "@/assets/js/rem.js";
 // 导入vant css(由于采用CDN导入这里无需导入)
-// import 'vant/lib/index.css' 
+// import 'vant/lib/index.css'
 // 路由权限拦截
-import './utils/permission'
+import "./utils/permission";
 // 初始化svg雪碧图
-import './icons'
+import "./icons";
 
 // vant 常用组件全局注册
-import { Button, Row, Col, Toast } from 'vant'
+import { Button, Row, Col, Toast } from "vant";
 Vue.use(Button)
   .use(Row)
   .use(Col)
-  .use(Toast)
+  .use(Toast);
 
 // 注册自定义全局组件
-import './utils/components'
+import "./utils/components";
 
 // 开发模式启动mock
-// console.log('开发模式启动mock')
-if (process.env != 'production') {
-  require('../mock')
-  console.log('本地mock数据已导入')
+if (
+  process.env.NODE_ENV == "development" ||
+  process.env.VUE_APP_MODE == "mock"
+) {
+  require("../mock");
+  console.log("本地mock数据已导入");
 }
-
 // 打印当前运行模式
-console.log('当前运行模式为:', process.env)
+console.log("当前运行模式为:", process.env);
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
