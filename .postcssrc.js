edcb6d7933d47b5bb02659dcbae06020120b439f
Maybe_QHL <1013480204@qq.com>
2020-11-04 11:08:06 +0800

    fix

--- .postcssrc.js
+++ .postcssrc.js
@@ -21,7 +21,7 @@ module.exports = ({ file }) => {
                 minPixelValue: 1, // 最小的转换数值，如果为1的话，只有大于1的值会被转换
                 mediaQuery: false, // 媒体查询里的单位是否需要转换单位
                 replace: true, // 是否直接更换属性值，而不添加备用属性
-                exclude: /node_modules/, // 忽略某些文件夹下的文件或特定文件
+                // exclude: /node_modules/, // 忽略某些文件夹下的文件或特定文件
                 include: undefined, // 如果设置了include，那将只有匹配到的文件才会被转换，例如只转换 'src/mobile' 下的文件 (include: /\/src\/mobile\//)
                 landscape: false, // 是否添加根据 landscapeWidth 生成的媒体查询条件 @media (orientation: landscape)
                 landscapeUnit: "vw", // 横屏时使用的单位
